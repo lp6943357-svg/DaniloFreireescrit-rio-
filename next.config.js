8d1966731f5fd4b365dcbd88b512f9937bfb28c4
Lucas <lp6943357@gmail.com>
2026-09-19 08:40:24 -0300

    Revisão técnica: configuração e SEO

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: { domains: ['localhost', 'images.unsplash.com'] },
+}
+module.exports = nextConfig
