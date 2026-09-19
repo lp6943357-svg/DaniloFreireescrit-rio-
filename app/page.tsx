44481e0d9f4d3327d8c76bb6836aa81b48909726
Lucas <lp6943357@gmail.com>
2026-09-19 08:41:08 -0300

    Revisão técnica: app e configuração

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Hero from '@/components/Hero'
+import About from '@/components/About'
+import Services from '@/components/Services'
+import Diferentials from '@/components/Diferentials'
+import Contact from '@/components/Contact'
+import Location from '@/components/Location'
+
+export default function Home() {
+  return <><Hero /><About /><Services /><Diferentials /><Location /><Contact /></>
+}
